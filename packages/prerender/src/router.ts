85d01d5b7e6fcaf657cd76794eb2d2c819fa46d8
Daniel Choudhury <dannychoudhury@gmail.com>
2021-01-22 15:00:27 +0000

    Attemp 1: Setup routes prerender babel plugin
    WIP: can't get past a couple of problems

--- packages/prerender/src/router.ts
+++ packages/prerender/src/router.ts
@@ -11,10 +11,39 @@ export const detectPrerenderRoutes = () => {
     .map((route) => ({
       name: route.name,
       path: route.path,
-      hasParams: route.hasParameters,
+      isPrivate: route.isPrivate,
       id: route.id,
+      whileLoading: route.whileLoadingAttr,
       filePath: route.page?.filePath,
     }))
 
   return prerenderRoutes
+}
+
+export const getSwitchStatementForPrerender = () => {
+  const lines: string[] = []
+  const rwProject = getProject(process.cwd())
+  const routes = rwProject.getRouter().routes
+
+  for (const route of routes) {
+    if (!route.path) continue
+    const componentToRenderForPath = route.isPrivate
+      ? route.whileLoadingAttr
+      : route.pageAttr
+    if (typeof componentToRenderForPath === 'undefined') continue
+    const key = route.path
+    lines.push(`
+		case ${JSON.stringify(key)}:
+			return (${componentToRenderForPath.getText()});
+		`)
+  }
+
+  return `
+	if (arguments.length > 0 && arguments[0].prerender){
+		switch(arguments[0].prerender?.path){
+			${lines.join('\n')}
+		}
+		return undefined
+	}
+	`
 }
