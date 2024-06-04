fa622d88ac8b1db9342a44e301ff6847d3539f9a
Daniel Choudhury <dannychoudhury@gmail.com>
2024-06-05 00:31:41 +0700

    Build cjs and mjs for rwjs/web

---
+++ packages/web/build.ts
@@ -0,0 +1,39 @@
+import { writeFileSync } from 'node:fs'
+
+import {
+  build,
+  defaultBuildOptions,
+  defaultIgnorePatterns,
+} from '@redwoodjs/framework-tools'
+
+// ESM build
+await build({
+  entryPointOptions: {
+    ignore: [...defaultIgnorePatterns, 'src/entry/**'],
+  },
+  buildOptions: {
+    ...defaultBuildOptions,
+    format: 'esm',
+    packages: 'external',
+  },
+})
+
+// CJS build
+await build({
+  entryPointOptions: {
+    ignore: [...defaultIgnorePatterns, 'src/entry/**'],
+  },
+  buildOptions: {
+    ...defaultBuildOptions,
+    outdir: 'dist/cjs',
+    packages: 'external',
+  },
+})
+
+// Place a package.json file with `type: commonjs` in the dist folder so that
+// all .js files are treated as CommonJS files.
+writeFileSync('dist/cjs/package.json', JSON.stringify({ type: 'commonjs' }))
+
+// Place a package.json file with `type: module` in the dist/esm folder so that
+// all .js files are treated as ES Module files.
+writeFileSync('dist/package.json', JSON.stringify({ type: 'module' }))
